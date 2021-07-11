a5acc8a724bd8452e0f980b2b457062d2a2c5777
jahanfarha3170 <farhatjahan42@gmail.com>
2021-07-11 15:40:56 -0700

    Pagination added in Product

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def index(request):
@@ -17,4 +19,11 @@ def display_product(request):
     if search_item != '' and search_item is not None:
         prod_object = Product.objects.filter(title__icontains=search_item)
 
-    return render(request, 'shop/products.html', {'prod_object': prod_object})
+    paginator = Paginator(prod_object, 1) #display 16 data/page
+    print("paginator.num_pages to see total pages after pagination", paginator.num_pages)
+    print("paginator.count to see total db product data before pagination", paginator.count)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    prod_object = paginator.get_page(page_number)
+
+    return render(request, 'shop/products.html', {'prod_object': prod_object, 'page_object': page_obj})
